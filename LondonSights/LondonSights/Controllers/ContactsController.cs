20698e273d31fd37ad4b23ccde0f1d5927c33fe1
AleksMorozova <aleks_morozova91@mail.ru>
2017-03-31 18:06:41 +0300

    First iteration for Contacts

---
+++ LondonSights/LondonSights/Controllers/ContactsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LondonSights.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            //get contacts from services 
+            return View();
+        }
+    }
+}
---
+++ LondonSights/Services/Implementation/ContactsService.cs
@@ -0,0 +1,19 @@
+﻿using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+
+namespace Services.Implementation
+{
+    public class ContactsService : IContactsService
+    {
+        public Contacts GetContacts()
+        {
+            //Get contacts from repository
+            return new Contacts();
+        }
+    }
+}
